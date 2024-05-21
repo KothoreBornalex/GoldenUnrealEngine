010c63b52578fbdaea0d1198a626cc5940250c90
Kothore <100525076+KothoreBornalex@users.noreply.github.com>
2024-05-21 13:57:50 +0200

    Test CPP + Directories Commit

---
+++ Source/GoldenUnrealEngine/Public/Core/CustomGameModeClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomGameModeClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLDENUNREALENGINE_API ACustomGameModeClass : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
