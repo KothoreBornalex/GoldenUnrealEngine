04850e9a09e91041d71bd9a5244b1e11b06419de
Kothore <100525076+KothoreBornalex@users.noreply.github.com>
2024-05-23 14:15:59 +0200

    [Add] Adding UEVSON Plugin for pathfinding

--- Source/GoldenUnrealEngine/GoldenUnrealEngine.Build.cs
+++ Source/GoldenUnrealEngine/GoldenUnrealEngine.Build.cs
@@ -8,6 +8,6 @@ public class GoldenUnrealEngine : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "WeaponsSystem", "EntitiesSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "WeaponsSystem", "EntitiesSystem", "UESVON" });
 	}
 }
